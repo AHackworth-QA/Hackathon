8f6382b53381b68164c1b422bb6fd236fa574ac4
sgoddardqa <73229946+sgoddardqa@users.noreply.github.com>
2021-02-02 14:37:12 +0000

    Test project for SQL Entity Framework project created

---
+++ VehicleMonitorTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VehicleMonitorTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
