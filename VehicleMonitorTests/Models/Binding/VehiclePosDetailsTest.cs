d32034725022d1d27a73ed228207e9c22c856f5c
sgoddardqa <73229946+sgoddardqa@users.noreply.github.com>
2021-02-03 11:36:23 +0000

    Created tests for binding models

---
+++ VehicleMonitorTests/Models/Binding/VehiclePosDetailsTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VehicleMonitor.Models.Binding;
+
+namespace VehicleMonitorTests.Models.Binding {
+
+    [TestClass]
+    class VehiclePosDetailsTest {
+
+        [TestMethod]
+        public void createVehiclePosDetailsTest() {
+            VehiclePosDetails vehiclePosDetails = new VehiclePosDetails() {
+                Longitude = 7,
+                Latitude = 9
+            };
+            Assert.AreEqual(vehiclePosDetails.Longitude, 7);
+            Assert.AreEqual(vehiclePosDetails.Latitude, 9);
+        }
+
+    }
+
+}
